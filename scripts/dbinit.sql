c52722d5c1c52f9983784ea54b04a09295fa7f79
Іван Брусняк <I.Brusniak@flowers.local>
2021-08-03 15:40:55 +0300

    Create table offices

--- scripts/dbinit.sql
+++ scripts/dbinit.sql
@@ -49,9 +49,21 @@ CREATE TABLE `employees` (
     KEY `reprostTo` (`reportsTo`),
     KEY `officeCode` (`officeCode`),
     CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`reportsTo`) REFERENCES `employees` (`employeeNumber`),
-    CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`officeCode`) REFERENCES `offices` (`officeCode`)
+    CONSTRAINT `employee_ibfk_2` FOREIGN KEY (`officeCode`) REFERENCES `offices` (`officeCode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `offices` (
+	`officeCode` varchar(10) NOT NULL,
+    `city` varchar(50) NOT NULL,
+    `phone` varchar(50) NOT NULL,
+    `addressLine1` varchar(50) NOT NULL,
+    `addressLine2` varchar(50) DEFAULT NULL,
+    `state` varchar(50) DEFAULT NULL,
+    `country` varchar(50) NOT NULL,
+    `postalCode` varchar(15) NOT NULL,
+    `territory` varchar(10) NOT NULL,
+    PRIMARY KEY (`officeCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 # Set initial value back to environment variable
 SET @@foreign_key_checks=@original_foreign_key_checks;
