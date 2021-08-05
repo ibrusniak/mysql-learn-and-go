81aec329d07b62dd5674b1da2323f8615eecdb20
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-05 19:11:31 +0300

    logic-operations - reserch logical MySQL operation

---
+++ scripts/basics/logic-operations.sql
@@ -0,0 +1,20 @@
+
+# Logic operations and ternary logic experiments
+
+SELECT TRUE AS `OPERANDS`
+	UNION
+SELECT FALSE
+	UNION
+SELECT NULL;
+
+SELECT 'AND' AS `OPERATIONS`
+	UNION
+SELECT 'OR'
+	UNION
+SELECT 'NOT'
+	UNION
+SELECT 'XOR';
+
+# Normal logical operations
+
+
