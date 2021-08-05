177748631cffb182990dfc2bf2a7e40145636b49
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-05 20:28:51 +0300

    modified select1.sql

---
+++ scripts/basics/select1.sql
@@ -0,0 +1,26 @@
+
+# SELECT select_list
+# FROM table_name;
+
+DESCRIBE `classicmodels`.`employees`;
+
+# SHOW CREATE TABLE `classicmodels`.`employees`;
+
+SELECT
+	`employeeNumber`,
+    `reportsTo`,
+    `lastName`,
+    `firstName`
+FROM
+	`classicmodels`.`employees`;
+    
+SELECT
+	concat_ws(', ', 'Dog', 'Frog', 'Cat');
+    
+SELECT
+	concat('Cat', 'Dog', 'Frog');
+
+SELECT NOW();
+
+SELECT NOW() - 60;
+
