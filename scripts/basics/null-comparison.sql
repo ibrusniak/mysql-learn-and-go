4299e8c519fda39d4bb52ded5ce765945abec738
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-07 07:29:56 +0300

    null comparison

---
+++ scripts/basics/null-comparison.sql
@@ -0,0 +1,19 @@
+
+SELECT
+	TRUE,
+    NULL,
+    FALSE;
+
+SELECT
+	NULL = NULL,
+    NULL <> NULL,
+    NULL > NULL,
+    NULL < NULL,
+    NULL <= NULL,
+    NULL >= NULL,
+    NULL IS NULL,
+    NULL IS NOT NULL,
+    NULL IN (NULL, NULL, NULL),
+    FIELD(NULL, 1, 2, 3, NULL);
+    
+    
