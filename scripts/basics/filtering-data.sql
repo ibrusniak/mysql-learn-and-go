f9b9e588ccf93376ac6d07a546059dc32bc92fd8
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-07 08:41:17 +0300

    filtering

---
+++ scripts/basics/filtering-data.sql
@@ -0,0 +1,33 @@
+
+# In MySQL, a predicate is a Boolean expression that evaluates to TRUE, FALSE, of UNKNOWN
+# Besides the SELECT statement, you can use the WHERE clause in the UPDATE or DELETE statement to
+# specify which rows to update or delete
+
+# FROM -> WHERE -> SELECT -> ORDER BY
+
+SELECT `field`
+FROM
+	(SELECT NULL AS `field` UNION ALL
+	SELECT 1 UNION
+	SELECT 2) AS `someTable`
+WHERE `field` IS NOT NULL
+ORDER BY `field` DESC
+LIMIT 2;
+
+
+SELECT
+	orderNumber,
+	orderDate
+FROM
+	`classicmodels`.`orders`
+ORDER BY
+	orderDate DESC;
+
+SELECT
+	orderNumber,
+    orderDate
+FROM
+	`classicmodels`.`orders`
+ORDER BY
+	orderDate DESC
+LIMIT 5;
