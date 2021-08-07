2bd3a786e1ce4db476f6e489d785cae138e9b915
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-07 07:23:55 +0300

    fix file

--- scripts/basics/field-function.sql
+++ scripts/basics/field-function.sql
@@ -1,5 +1,6 @@
 
-SELECT FIELD('A', 'D', 'F', 'A', 'C', '4', 'B', 'B');
+SELECT
+	FIELD('A', 'D', 'F', 'A', 'C', '4', 'B', 'B');
 
 # NULL can not be compared normally with '=', only 'IS NULL'
 SELECT FIELD(NULL, FALSE, TRUE, FALSE, '2', 200, NULL, 0);
