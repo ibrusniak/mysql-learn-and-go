04a5acc2880f353dc61cce0ca92489183d08469b
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-10 07:49:52 +0300

    create table `book`

--- scripts/joining/join.sql
+++ scripts/joining/join.sql
@@ -12,15 +12,20 @@
 CREATE DATABASE IF NOT EXISTS `library` DEFAULT CHARSET = utf8mb4;
 USE `library`;
 
-CREATE TABLE IF NOT EXISTS `book` (
-  `id` LONG NOT NULL AUTO_INCREMENT,
+SET @@foreign_key_checks = 0;
+
+DROP TABLE IF EXISTS `book`;
+
+CREATE TABLE `book` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(200) NOT NULL,
-  `author` LONG NOT NULL,
-  `short_description` TEXT DEFAULT 'empty',
+  `author_id` INT NOT NULL,
+  `short_description` TEXT,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`author`) REFERENCES `author` (`id`) ON DELETE CASCADE
+  FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE CASCADE
 );
 
+/*
 CREATE TABLE IF NOT EXISTS `author`;
 CREATE TABLE IF NOT EXISTS `country`;
 CREATE TABLE IF NOT EXISTS `state`;
@@ -29,4 +34,6 @@ CREATE TABLE IF NOT EXISTS `floor`;
 CREATE TABLE IF NOT EXISTS `room`;
 CREATE TABLE IF NOT EXISTS `rack`;
 CREATE TABLE IF NOT EXISTS `shelf`;
+*/
 
+SET @@foreign_key_checks = 1;
