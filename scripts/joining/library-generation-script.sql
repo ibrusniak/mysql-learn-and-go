29ec54987d7b067d1c9d51d70185bea9acab4e6f
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-11 09:08:15 +0300

    remove `\t` chars from file

--- scripts/joining/library-generation-script.sql
+++ scripts/joining/library-generation-script.sql
@@ -20,36 +20,36 @@ DROP TABLE IF EXISTS `country`;
 DROP TABLE IF EXISTS `booklocation`;
 
 CREATE TABLE `book` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(200) NOT NULL,
-	`author_id` INT NOT NULL,
-	`short_description` TEXT,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(200) NOT NULL,
+    `author_id` INT NOT NULL,
+    `short_description` TEXT,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `author`(
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`first_name` VARCHAR(100) NOT NULL,
-	`last_name` VARCHAR(100) NOT NULL,
-	`middle_name` VARCHAR(100) DEFAULT NULL,
-	`date_of_birth` DATETIME NOT NULL,
-	`date_of_death` DATETIME DEFAULT NULL,
-	`country_of_birth_id` INT DEFAULT NULL,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`country_of_birth_id`) REFERENCES `country` (`id`) ON DELETE NO ACTION
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(100) NOT NULL,
+    `last_name` VARCHAR(100) NOT NULL,
+    `middle_name` VARCHAR(100) DEFAULT NULL,
+    `date_of_birth` DATETIME NOT NULL,
+    `date_of_death` DATETIME DEFAULT NULL,
+    `country_of_birth_id` INT DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`country_of_birth_id`) REFERENCES `country` (`id`) ON DELETE NO ACTION
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `country` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`title` VARCHAR(200) NOT NULL,
-	`short_description` TEXT,
-	PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(200) NOT NULL,
+    `short_description` TEXT,
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `booklocation` (
-	`book_id` INT NOT NULL,
+    `book_id` INT NOT NULL,
     `floor_number` INT NOT NULL,
     `rack_number` INT NOT NULL,
     `shelf_number` INT NOT NULL,
