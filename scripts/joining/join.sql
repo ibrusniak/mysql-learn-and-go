e3ef3f976c1e6a5c3bbd2233ab3b66c165b1fc48
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-11 08:50:17 +0300

    simplify tables. Create table `country`

--- scripts/joining/join.sql
+++ scripts/joining/join.sql
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `floor`;
 DROP TABLE IF EXISTS `shelf`;
 
 CREATE TABLE `book` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(200) NOT NULL,
-  `author_id` INT NOT NULL,
-  `short_description` TEXT,
-  PRIMARY KEY (`id`),
-  FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(200) NOT NULL,
+	`author_id` INT NOT NULL,
+	`short_description` TEXT,
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`author_id`) REFERENCES `author` (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
 
@@ -40,19 +40,18 @@ CREATE TABLE IF NOT EXISTS `author`(
 	`date_of_birth` DATETIME NOT NULL,
 	`date_of_death` DATETIME DEFAULT NULL,
 	`country_of_birth_id` INT DEFAULT NULL,
-	`state_of_birth_id` INT DEFAULT NULL,
-	`city_of_birth_id`	INT DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`country_of_birth_id`) REFERENCES `country` (`id`) ON DELETE NO ACTION,
-    FOREIGN KEY (`state_of_birth_id`) REFERENCES `state` (`id`) ON DELETE NO ACTION,
-    FOREIGN KEY (`city_of_birth_id`) REFERENCES `city` (`id`) ON DELETE NO ACTION
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`country_of_birth_id`) REFERENCES `country` (`id`) ON DELETE NO ACTION
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 
-/*
-CREATE TABLE IF NOT EXISTS `country`;
-CREATE TABLE IF NOT EXISTS `state`;
+CREATE TABLE IF NOT EXISTS `country` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(200) NOT NULL,
+	`short_description` TEXT,
+	PRIMARY KEY (`id`)
+);
 
-CREATE TABLE IF NOT EXISTS `city`;
+/*
 CREATE TABLE IF NOT EXISTS `floor`;
 CREATE TABLE IF NOT EXISTS `room`;
 CREATE TABLE IF NOT EXISTS `rack`;
