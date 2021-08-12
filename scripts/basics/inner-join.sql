32082493de77e7f56e98a748b1a5581a1ae1af1c
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-12 07:19:13 +0300

    reorganize codebase

---
+++ scripts/basics/inner-join.sql
@@ -0,0 +1,52 @@
+
+
+CREATE DATABASE IF NOT EXISTS `dbtest0`;
+
+USE dbtest0;
+
+DROP TABLE IF EXISTS T1;
+DROP TABLE IF EXISTS T2;
+
+CREATE TABLE T1 (
+    id INT,
+    color VARCHAR(10)
+);
+
+CREATE TABLE T2 (
+    id INT,
+    color VARCHAR(10)
+);
+
+INSERT INTO T1 (id, color)
+VALUES
+    (1, 'green'),
+    (2, 'red'),
+    (3, 'blue'),
+    (NULL, 'magenta'),
+    (NULL, 'pink'),
+    (NULL, NULL),
+    (NULL, NULL); 
+    
+INSERT INTO T2 (id, color)
+VALUES
+    (1, 'green'),
+    (2, 'red'),
+    (3, 'blue'),
+    (NULL, 'magenta'),
+    (NULL, 'pink'),
+    (NULL, NULL),
+    (NULL, NULL); 
+
+SELECT * FROM T1, T2;                           # 7 * 7 = 49 rows selected
+SELECT * FROM T1 INNER JOIN T2 ON TRUE;         # 49 rows
+
+SELECT * FROM T1 INNER JOIN T2 ON FALSE;                                # empty set 
+SELECT * FROM T1 INNER JOIN T2 ON 0;                                    # empty set 
+SELECT * FROM T1 INNER JOIN T2 ON '';                                   # empty set 
+SELECT * FROM T1 INNER JOIN T2 ON "";                                   # empty set 
+SELECT * FROM T1 INNER JOIN T2 ON CAST('0000-00-00' AS DATE);           # empty set 
+SELECT * FROM T1 INNER JOIN T2 ON NULL;                                 # empty set 
+
+SELECT * FROM t1 INNER JOIN t2 USING(color, id); # ONLTY 3 ROWS. Because null can not be compare with true
+SELECT * FROM t1 INNER JOIN t2 on (t1.id = t2.id) or (t2.id = 2);
+
--- scripts/basics/self-join.sql
+++ scripts/basics/self-join.sql
@@ -1,15 +1,7 @@
 
-# MySQL supports:
-#
-# Self
-# Inner
-# Left
-# Right
-# Cross
 
-# DEMO TABLE for `self join`
-/*
 CREATE DATABASE IF NOT EXISTS `dbtest0`;
+
 USE `dbtest0`;
 
 DROP TABLE IF EXISTS `employees`;
@@ -45,14 +37,9 @@ VALUES
     ("Samira", "Gilmour" , 1),
     ("Lana", "Mcfadden" , 1),
     ("Kris", "Lang" , 1);
-
-
 UPDATE `employees` SET `reportsTo` = 2 WHERE `id` IN (3, 4, 5);
-
 UPDATE `employees` SET `reportsTo` = 6 WHERE `id` IN (7, 8, 9);
-
 # self join
-
 SELECT
     `emp1`.`id` AS `Employee Id`,
     `emp1`.`first_name` AS `Employee first name`,
@@ -67,54 +54,4 @@ FROM
 WHERE
     `emp1`.`reportsTo` = `emp2`.`id`
 ORDER BY
-    `emp1`.`id`
-;
-*/
-# inner join
-
-USE dbtest0;
-DROP TABLE IF EXISTS T1;
-DROP TABLE IF EXISTS T2;
-
-CREATE TABLE T1 (
-    id INT,
-    color VARCHAR(10)
-);
-
-CREATE TABLE T2 (
-    id INT,
-    color VARCHAR(10)
-);
-
-INSERT INTO T1 (id, color)
-VALUES
-    (1, 'green'),
-    (2, 'red'),
-    (3, 'blue'),
-    (NULL, 'magenta'),
-    (NULL, 'pink'),
-    (NULL, NULL),
-    (NULL, NULL); 
-    
-INSERT INTO T2 (id, color)
-VALUES
-    (1, 'green'),
-    (2, 'red'),
-    (3, 'blue'),
-    (NULL, 'magenta'),
-    (NULL, 'pink'),
-    (NULL, NULL),
-    (NULL, NULL); 
-
-SELECT * FROM T1, T2;                           # 7 * 7 = 49 rows selected
-SELECT * FROM T1 INNER JOIN T2 ON TRUE;         # 49 rows
-
-SELECT * FROM T1 INNER JOIN T2 ON FALSE;                                # empty set 
-SELECT * FROM T1 INNER JOIN T2 ON 0;                                    # empty set 
-SELECT * FROM T1 INNER JOIN T2 ON '';                                   # empty set 
-SELECT * FROM T1 INNER JOIN T2 ON "";                                   # empty set 
-SELECT * FROM T1 INNER JOIN T2 ON CAST('0000-00-00' AS DATE);           # empty set 
-SELECT * FROM T1 INNER JOIN T2 ON NULL;                                 # empty set 
-
-SELECT * FROM t1 INNER JOIN t2 USING(color, id); # ONLTY 3 ROWS. Because null can not be compare with true
-SELECT * FROM t1 INNER JOIN t2 on (t1.id = t2.id) or (t2.id = 2);
+    `emp1`.`id`;
