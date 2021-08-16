ddde81e24af36d4ba50ef28e68ec7fec9c7779ae
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-16 12:42:30 +0300

    common use of CTE

---
+++ scripts/basics/cte-1.sql
@@ -0,0 +1,46 @@
+
+# Commin Table Expression (MySQL version 8+)
+
+# Similar to a derived table, a CTE is not stored as an
+# object and last only during the execution of a query.
+
+# Unlike a derived table, a CTE can be self-referencing (a recursive CTE)
+# or can be referenced multiple times in the same query.
+
+with tab1 (n, m) as (
+    select 2 num, 'c' m union all
+    select 3, 'd' union all
+    select 1, 'q'
+)
+select
+    *
+from
+    tab1;
+
+with cte1 (a, p, ctename) as (
+    select 2, 'st', 'cte1'
+),
+cte2 (q, d, ctename) as (
+    select 100, 'dd', 'cte2'
+),
+cteN (f, h, ctename) as (
+    select 300 , 'f', 'cteN'
+)
+select a, p, ctename from cte1 union all
+select q, d, ctename from cte2 union all
+select f, h, ctename from cteN;
+
+use classicmodels;
+
+with cte0 (a, b) as (select 2, 3),
+cte1 (a, b) as (select a, b from cte0),
+cte2 (a, b) as (select a, b from cte1)
+select * from cte2;
+
+/*
+-- error "no database selected"
+with cte5 (a) as (select a from cte6),
+cte6 (a) as (select 2)
+select * from cte5; */
+
+
