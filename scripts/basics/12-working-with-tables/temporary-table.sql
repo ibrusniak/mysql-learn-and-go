10b730f5f5e334f764625035462f23037c25601a
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-25 07:21:49 +0300

    `overloading` permanent table with temporary

---
+++ scripts/basics/12-working-with-tables/temporary-table.sql
@@ -0,0 +1,17 @@
+
+create database if not exists use_temporary_tables;
+use use_temporary_tables;
+
+create /* permanent */ table test (
+  c1 int
+);
+
+-- `oveloaded`. Afrer create temp table with
+-- same name as permanent - can not acces to
+-- permanent table...
+create temporary table test (
+  c1 varchar(10),
+  c2 int
+);
+
+describe test; # temporary
