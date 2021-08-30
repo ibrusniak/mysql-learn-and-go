17feff56ea942583d2337e55404a03e13f3c1680
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-08-30 09:43:45 +0300

    one to many

--- scripts/basics/14-constraints/foreign-key.sql
+++ scripts/basics/14-constraints/foreign-key.sql
@@ -37,5 +37,48 @@ create table t1 (
     constraint t1_pk primary key (pk)
 );
 
+-- `one to many`
+create table t_parent (
+    pk int primary key auto_increment,
+    c1 int
+);
+
+create table t_child (
+    pk int primary key auto_increment,
+    c1 int,
+    t_parent_pk int,
+    constraint t_child_pk foreign key (t_parent_pk) references t_parent (pk)
+);
+
+alter table t_child
+change column
+c1 ch1_c1 int;
+
+insert into t_parent (c1)
+values
+    (100), (200), (300)
+;
+
+insert into t_child (ch1_c1, t_parent_pk)
+values
+    ((select round(rand() * 1000)), 100),
+    ((select round(rand() * 1000)), 100),
+    ((select round(rand() * 1000)), 100),
+    ((select round(rand() * 1000)), 300),
+    ((select round(rand() * 1000)), 300),
+    ((select round(rand() * 1000)), 100),
+    ((select round(rand() * 1000)), 100),
+    ((select round(rand() * 1000)), 100)
+;
+
+select
+    t_parent.pk `parent primary key`,
+    t_parent.c1 `parent c1`,
+    t_child.pk `child primary key`,
+    t_child.t_parent_pk `child foreign key`,
+    t_child.ch1_c1 `child c1`
+from
+    t_parent inner join t_child
+    on t_parent.c1 = t_child.t_parent_pk;
 
 set @@foreign_key_checks = 1;
