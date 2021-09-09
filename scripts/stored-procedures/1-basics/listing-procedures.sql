a39388135a0fd488ec9c8b07f5419833b1a25b20
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-09-09 15:32:41 +0300

    listing procedures

---
+++ scripts/stored-procedures/1-basics/listing-procedures.sql
@@ -0,0 +1,10 @@
+
+show procedure status;
+
+-- show create table information_schema.routines;
+describe information_schema.routines;
+select * from information_schema.routines;
+
+show procedure status where db = 'adb';
+show procedure status where db = 'routinegames';
+
