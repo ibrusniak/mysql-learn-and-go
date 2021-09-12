a0b83b1887e2947a17f6aad2061a27f37a584cd6
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-09-12 08:07:55 +0300

    my own `debugger` is debugged

--- scripts/stored-procedures/2-conditional-stmt/case-2.sql
+++ scripts/stored-procedures/2-conditional-stmt/case-2.sql
@@ -3,39 +3,64 @@ drop database if exists testdb;
 create database testdb;
 use testdb;
 
-create temporary table if not exists  debug_data (
-  tstamp timestamp default current_timestamp(),
+create temporary table if not exists  debug_log (
+  _message_num int unsigned auto_increment primary key,
+  _timestamp timestamp default current_timestamp(),
   _val int,
   _comment varchar(200)
 );
 
-truncate debug_data;
+truncate debug_log;
 
 delimiter $$
 
-create procedure write_debug(in val int, in message varchar(200))
+create procedure debug_writer(in val int, in message varchar(200))
 begin
-  insert into debug_data (_val, _comment)
+  insert into debug_log (_val, _comment)
   values (val, message);
 end$$
 
 create procedure p1(in a int)
 begin
-  call write_debug(a, 'p1. in');
+  call debug_writer(a, 'p1. in 1');
+  -- outer 'a' is read only because of 'in a'
+  set a = a + 150;
+  call debug_writer(a, 'p1. in 2');
 end$$
 
 create procedure p2(out b int)
 begin
-  call write_debug(a, 'p2. out');
+  call debug_writer(b, 'p2. out. \'b\' before increment');
+  set b = b + 500;
+  call debug_writer(b, 'p2. out. \'b\' after increment');
 end$$
 
 create procedure p3(inout c int)
 begin
-  call write_debug(c, 'p3. inout');
+  call debug_writer(c, 'p3. inout. \'c\' before increment');
+  set c = c + 1000;
+  call debug_writer(c, 'p3. inout. \'c\' after increment');
 end$$
 
 delimiter ;
 
+set @va = 100;
+call debug_writer(@va, '@va initial');
+call p1(@va);
+call debug_writer(@va, '@va after call p1 (in a)');
 
+set @vb = 200;
+call debug_writer(@vb, '@vb initial');
+call p2(@vb);
+-- initial value of @vb lost inside of 'p2' :(
+call debug_writer(@vb, '@vb after call p2 (out b)');
+
+set @vc = 50;
+call debug_writer(@vc, '@vc initial');
+call p3(@vc);
+call debug_writer(@vc, '@vc after call pc (inout c)');
+
+
+select * from debug_log;
 
 
