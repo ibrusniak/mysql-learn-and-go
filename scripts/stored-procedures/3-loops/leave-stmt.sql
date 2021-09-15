3198d9e84b61c73b643f207087eafcc51a98ec82
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-09-15 07:30:23 +0300

    use `leave`. first case

---
+++ scripts/stored-procedures/3-loops/leave-stmt.sql
@@ -0,0 +1,29 @@
+
+drop database if exists choomba_db;
+create database choomba_db;
+use choomba_db;
+
+delimiter $$
+
+create procedure leave_demo_1(in val tinyint unsigned)
+label: begin
+    if 2 = val
+      then
+        call debug_log(1, val);
+        leave label;
+    end if;
+    call debug_log(2, val);
+end$$
+
+create procedure debug_log(
+  in _point tinyint unsigned, in val tinyint unsigned
+)
+begin
+  select _point `debug point`, val `'val' value`;
+end$$
+
+delimiter ;
+
+call leave_demo_1(2);
+call leave_demo_1(3);
+
