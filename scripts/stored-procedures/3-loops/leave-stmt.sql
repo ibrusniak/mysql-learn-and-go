2c565e832a84de645c5b88c1d8133a12df4afb05
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-09-15 09:20:07 +0300

    use `leave` stmt

--- scripts/stored-procedures/3-loops/leave-stmt.sql
+++ scripts/stored-procedures/3-loops/leave-stmt.sql
@@ -5,6 +5,13 @@ use choomba_db;
 
 delimiter $$
 
+create procedure debug_log(
+  in _point tinyint unsigned, in val tinyint unsigned
+)
+begin
+  select _point `debug point`, val `'val' value`;
+end$$
+
 create procedure leave_demo_1(in val tinyint unsigned)
 label: begin
     if 2 = val
@@ -15,15 +22,38 @@ label: begin
     call debug_log(2, val);
 end$$
 
-create procedure debug_log(
-  in _point tinyint unsigned, in val tinyint unsigned
-)
+create procedure leave_demo_2()
+label_3: begin
+    label_2: begin
+        label_1: begin
+            call debug_log(1, null);
+            leave label_2;
+        end;
+        call debug_log(2, null);
+    end;
+    call debug_log(3, null);
+end$$
+
+create procedure leave_demo_3()
 begin
-  select _point `debug point`, val `'val' value`;
+    declare _counter tinyint unsigned default 0;
+    declare _result varchar(50) default '';
+    label_0: loop
+        set _counter = _counter + 1;
+        set _result = concat(_result, ' ', _counter);
+        if _counter = 10 then
+            leave label_0;
+        end if;
+    end loop;
+    select _result;
 end$$
 
 delimiter ;
 
-call leave_demo_1(2);
-call leave_demo_1(3);
+-- call leave_demo_1(2);
+-- call leave_demo_1(3);
+
+-- call leave_demo_2();
+
+call leave_demo_3();
 
