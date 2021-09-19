98617df110637d8370c6fb746624ea7921e5bec3
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-09-19 09:47:29 +0300

    use `function`. Trivial case 2

--- scripts/stored-procedures/6-stored-functions/stored-functions-1.sql
+++ scripts/stored-procedures/6-stored-functions/stored-functions-1.sql
@@ -44,10 +44,35 @@ delimiter ;
 set @initial_max_sp_recursion_depth = @@max_sp_recursion_depth;
 set @@max_sp_recursion_depth = 90;
 
-select
+-- select
   -- Error Code: 1424. Recursive stored functions and triggers are not allowed. :(
-  factorial(5) `factorial 5`;
+  -- factorial(5) `factorial 5`;
 
 set @@max_sp_recursion_depth = @initial_max_sp_recursion_depth;
 
+
+drop table if exists dummy_table_1;
+
+create table if not exists dummy_table_1 (
+  col1 int unsigned not null
+);
+
+delimiter $$
+
+-- drop function if exists GetRandom;
+create function GetRandom() returns int unsigned deterministic
+  return round(rand() * 100000) $$
+
+-- drop procedure if exists FillTableWithDummyData;
+create procedure FillTableWithDummyData(in count_of_rows int unsigned)
+begin
+  repeat
+    insert into dummy_table_1 values (GetRandom());
+    set count_of_rows = count_of_rows - 1;
+  until count_of_rows = 0 end repeat;
+end $$
+
+delimiter ;
+
+call FillTableWithDummyData(200);
 
