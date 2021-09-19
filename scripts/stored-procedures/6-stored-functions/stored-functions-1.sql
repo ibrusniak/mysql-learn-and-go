37ea92b2a27db17a187633b772905a8eca16331e
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-09-19 09:20:33 +0300

    recursive stored function are not allowed

--- scripts/stored-procedures/6-stored-functions/stored-functions-1.sql
+++ scripts/stored-procedures/6-stored-functions/stored-functions-1.sql
@@ -25,5 +25,29 @@ select
   f_get_random_int() 'random 4'
 ;
 
+delimiter $$
+
+
+create function factorial(num tinyint) returns bigint deterministic
+begin
+  if num = 0 then return 1;
+  elseif num = 1 then return 1;
+  elseif num > 90 then
+    signal sqlstate '99999' set message_text = 'Too big input parameter!';
+  else
+    return num * factorial(num - 1);
+  end if;
+end $$
+
+delimiter ;
+
+set @initial_max_sp_recursion_depth = @@max_sp_recursion_depth;
+set @@max_sp_recursion_depth = 90;
+
+select
+  -- Error Code: 1424. Recursive stored functions and triggers are not allowed. :(
+  factorial(5) `factorial 5`;
+
+set @@max_sp_recursion_depth = @initial_max_sp_recursion_depth;
 
 
