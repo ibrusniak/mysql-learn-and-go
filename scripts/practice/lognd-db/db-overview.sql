6c4572c6717e759ab5e6ac5d0cd13d0d9413d028
Ivan Brusniak <ivan.brusniak@gmail.com>
2021-09-26 13:56:48 +0300

    verify db data

---
+++ scripts/practice/lognd-db/db-overview.sql
@@ -0,0 +1,27 @@
+
+USE long_db;
+
+SHOW TABLES;
+
+DESCRIBE `countries`;
+DESCRIBE `manufacturers`;
+DESCRIBE `orders`;
+DESCRIBE `phones`;
+DESCRIBE `products`;
+DESCRIBE `users`;
+
+SHOW INDEXES IN `countries`;
+SHOW INDEXES IN `manufacturers`;
+SHOW INDEXES IN `orders`;
+SHOW INDEXES IN `phones`;
+SHOW INDEXES IN `products`;
+SHOW INDEXES IN `users`;
+
+SELECT *  FROM `countries`;
+SELECT *  FROM `manufacturers`;
+SELECT *  FROM `orders`;
+SELECT *  FROM `phones`;
+SELECT *  FROM `products`;
+SELECT *  FROM `users`;
+
+
